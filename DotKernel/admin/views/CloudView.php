bf58c11be49a01a7dcace00e077537bd73a1d5fd
adrianacapata <capataadriana@yahoo.com>
2017-05-04 16:25:47 +0300

    added CloudView

--- DotKernel/admin/views/CloudView.php
+++ DotKernel/admin/views/CloudView.php
@@ -8,6 +8,24 @@ class Cloud_View extends View
 		$this->registry = Zend_Registry::getInstance();
 		$this->tpl = $tpl;
 	}
+	
+	
+	private function _setData($data, $prefix='')
+	{
+		if(!is_array($data))
+		{
+			// ver 1
+			error_log("NOT AN ARRAY ". __FILE__ .':'.__LINE__);
+			return false;
+			// ver 2 - won't happen because of the return
+			// throw new Exception("Not an array passed");
+		}
+		foreach ($data as $key => $value)
+		{
+			$this->tpl->setVar(strtoupper($prefix.$key) , $value);
+		}
+		return true;
+	}
+	
+	
 }
-
-class 
